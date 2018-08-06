36b14a22b70221f96c8aa491ae06d1349637eeef
Terje Nagel <terje.nagel@gmail.com>
2018-08-07 00:04:14 +0200

    Updated deploy script to include PS Core and Windows PS

--- Shell/Powershell/Deploy-ProfileModule.ps1
+++ Shell/Powershell/Deploy-ProfileModule.ps1
@@ -1,10 +1,16 @@
 try {
-    $moduleFolder = $Env:USERPROFILE + "\Documents\PowerShell\Modules";
-    $profileModuleFolder = $moduleFolder + "\ProfileModule"
-    New-Item -ItemType Directory -Force -Path $moduleFolder | Out-Null
-    if(Test-Path $profileModuleFolder) { Remove-Item -Path $profileModuleFolder -Recurse | Out-Null }
-    Copy-Item .\ProfileModule $moduleFolder -Recurse | Out-Null
-    Write-Host -ForegroundColor DarkYellow "ProfileModule updated"
+    function Deploy($location)
+    {
+        $moduleFolder = $Env:USERPROFILE + "\Documents\$location\Modules";
+        $profileModuleFolder = $moduleFolder + "\ProfileModule"
+        New-Item -ItemType Directory -Force -Path $moduleFolder | Out-Null
+        if(Test-Path $profileModuleFolder) { Remove-Item -Path $profileModuleFolder -Recurse | Out-Null }
+        Copy-Item .\ProfileModule $moduleFolder -Recurse | Out-Null
+        Write-Host -ForegroundColor DarkYellow "ProfileModule updated"
+    }
+
+    Deploy("PowerShell")
+    Deploy("WindowsPowerShell")
 }
 catch {
     Write-Host -ForegroundColor DarkRed "An error occcured"
