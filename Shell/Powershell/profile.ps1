55698cc92e53d3d35b46778a6d97b6adea5024a2
Terje Nagel <tna@schultz.dk>
2019-04-12 11:36:45 +0200

    Updated STart-TestShell to take optional module name

--- Shell/Powershell/profile.ps1
+++ Shell/Powershell/profile.ps1
@@ -1,14 +1,21 @@
 
 Import-Module ProfileModule
 
-function Start-TestShell {
-    pwsh -NoExit -NoProfile -Command {
-        Import-Module ProfileModule
-        function prompt { 
-            Write-Host -NoNewline -ForegroundColor Green "$($pwd.Path.Substring($pwd.Path.LastIndexOf("\"))) TEST";
-            return ">"
-        }
+function Start-TestShell ([string]$Module)
+{
+  pwsh -NoExit -NoProfile -Command {
+    param($Module)
+    Import-Module ProfileModule | Out-Null
+    if($Module)
+    {
+      Import-Module -Name $Module
+      Write-Host "Imported module: " $Module
     }
+    function prompt { 
+      Write-Host -NoNewline -ForegroundColor Green "$($pwd.Path.Substring($pwd.Path.LastIndexOf("\"))) TEST";
+      return ">"
+    }
+  } -args $Module
 }
 
 Set-Alias -Name sts -Value Start-TestShell
