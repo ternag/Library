cd278a39612aab01ff6c041abe5aca941e862f5e
Terje Nagel <terje.nagel@gmail.com>
2019-04-21 17:42:11 +0200

    Updated profile template

--- Shell/Powershell/profile.ps1
+++ Shell/Powershell/profile.ps1
@@ -1,14 +1,25 @@
 
-Import-Module ProfileModule
+Import-Module ProfileModule -WarningAction SilentlyContinue
 
-function Start-TestShell {
-    pwsh -NoExit -NoProfile -Command {
-        Import-Module ProfileModule
-        function prompt { 
-            Write-Host -NoNewline -ForegroundColor Green "$($pwd.Path.Substring($pwd.Path.LastIndexOf("\"))) TEST";
-            return ">"
-        }
+# install posh-git by running:
+# > PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+# Import-Module posh-git
+
+function Start-TestShell ([string]$Module)
+{
+  pwsh -NoExit -NoProfile -Command {
+    param($Module)
+    Import-Module ProfileModule -WarningAction SilentlyContinue
+    if($Module)
+    {
+      Import-Module -Name $Module
+      Write-Host "Imported module: " $Module
     }
+    function prompt { 
+      Write-Host -NoNewline -ForegroundColor Green "$($pwd.Path.Substring($pwd.Path.LastIndexOf("\"))) TEST";
+      return ">"
+    }
+  } -args $Module
 }
 
 Set-Alias -Name sts -Value Start-TestShell
