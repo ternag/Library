f6588a9bde9b9b203ed0fd006e04f61789066948
ternag <ternag@users.noreply.github.com>
2020-03-14 16:28:18 +0100

    Add Force attribute to la command
    Add Force attribute to la command to force list of hidden files

--- Shell/Powershell/ProfileModule/ProfileModule.psm1
+++ Shell/Powershell/ProfileModule/ProfileModule.psm1
@@ -1,6 +1,6 @@
 function ll { param ([String]$path = ".") Get-ChildItem -Path $path -Exclude .* }
 
-function la { param ([String]$path = ".") Get-ChildItem -Path $path }
+function la { param ([String]$path = ".") Get-ChildItem -Path $path -Force}
 
 function Write-Info { param ([string]$message) Write-Host -ForegroundColor DarkYellow $message}
 function Write-Error { param ([string]$message) Write-Host -ForegroundColor DarkRed $message}
